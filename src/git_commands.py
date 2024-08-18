15a0a72956739e0ac1757b0e42c0942b987b3148
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-08-18 15:12:24 +0100

    Added git init command functionality

--- src/git_commands.py
+++ src/git_commands.py
@@ -2,7 +2,12 @@ import os
 
 
 def init():
-    pass
+    os.mkdir(".git")
+    os.mkdir(".git/objects")
+    os.mkdir(".git/refs")
+    with open(".git/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+    print("Initialized git directory")
 
 
 def cat_file():
